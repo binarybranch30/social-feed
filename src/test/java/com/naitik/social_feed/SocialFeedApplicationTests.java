800cdb332ca3303d1de19956e0f907f7976c9ced
Naitik Srivastava <naitiksri08@.com>
2026-09-05 13:53:01 +0530

    Setting up Git

---
+++ src/test/java/com/naitik/social_feed/SocialFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naitik.social_feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
